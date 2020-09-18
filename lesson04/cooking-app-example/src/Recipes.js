44c8b002ae4a01fe183cc88ea89010a17a7eb34d
Kristian <krdo@eaaa.dk>
2020-09-18 09:07:31 +0200

    Additions to the cooking receipe app

--- lesson04/cooking-app-example/src/Recipes.js
+++ lesson04/cooking-app-example/src/Recipes.js
@@ -4,13 +4,18 @@ import {Link} from "@reach/router";
 function Recipes(props) {
   let recipes = props.recipes;
 
+  let mapFunction = element => 
+    <Link to={`/recipe/${element.id}`} key={element.id}>
+      <li>{element.title}</li>
+    </Link>;
+
+  let list = recipes.map(mapFunction);
+
   return (
     <>
       <h3>Recipes</h3>
       <ul>
-        {recipes.map(element => 
-          <Link to={`/recipe/${element.id}`} key={element.id}><li>{element.title}</li></Link>
-        )}
+        {list}
       </ul>
     </>
   );
