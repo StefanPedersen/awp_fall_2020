4e83e7012ad8e6b618d5f9ae32a87828bb4efdbe
Kristian Dorland <kristian@dorland.dk>
2020-11-19 14:47:00 +0100

    Fixing qa exercise

--- lesson13/qa-exercise/qa_app.js
+++ lesson13/qa-exercise/qa_app.js
@@ -1,10 +1,10 @@
-const {createStore} = require('redux');
-const {reducer} = require('./qa_reducer');
+const { createStore } = require('redux');
+const { reducer } = require('./qa_reducer');
 
 const store = createStore(reducer);
 store.subscribe(() => {
-    store.getState().forEach(o => console.log(o));
-    console.log("*******");
+  store.getState().forEach(o => console.log(o));
+  console.log("*******");
 });
 
 // TODO: dispatch some actions to demonstrate that this app works!
