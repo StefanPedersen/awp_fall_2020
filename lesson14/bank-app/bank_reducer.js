cadc0dc64f55d7521d9052d01a9d16566e76831e
Kristian Dorland <kristian@dorland.dk>
2020-11-26 10:47:54 +0100

    Work on lesson14 code

--- lesson14/bank-app/bank_reducer.js
+++ lesson14/bank-app/bank_reducer.js
@@ -1,11 +1,13 @@
 function reducer(state = { amount: 0 }, action) {
   switch (action.type) {
     case 'DEPOSIT': {
-      if (action.amount <= 0) return { amount: state.amount };
+      if (action.amount <= 0) 
+        return { amount: state.amount };
       else return { amount: state.amount + action.amount };
     }
     case 'WITHDRAW': {
-      if (action.amount <= 0 || action.amount > state.amount) return { amount: state.amount };
+      if (action.amount <= 0 || action.amount > state.amount) 
+        return { amount: state.amount };
       else return { amount: state.amount - action.amount };
     }
     default:
